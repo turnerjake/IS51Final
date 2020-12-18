1edc57ea3b618bea25d39bc30d6fdf9c3155bbbb
turnerjake <jaketurner@mail.fresnostate.edu>
2020-12-17 18:07:45 -0800

    structured-english

---
+++ final_exam.py
@@ -0,0 +1,12 @@
+"""
+STRUCTURED ENGLISH
+
+Open final.txt
+Read and print the number of grades in the text file
+Calculate and print the average of the grades
+Find the number of grades that are above the average we just calculated
+Divide this count by the total number of grades to find the percent that are above the average
+Print this percentage to user
+
+END STRUCTURED ENGLISH
+"""
